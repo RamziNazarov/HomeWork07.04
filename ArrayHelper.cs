35853b6b0d46b1f698364fb04c6ce09aa8cdc080
Ramz Nazarov <Ramzi.13.09.18@gmail.com>
2020-04-08 06:26:13 +0300

    add new method Pop with 5 override

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -3,6 +3,35 @@ namespace HomeWork._07._04
 {
     static class ArrayHelper
     {
-        
+        public static string Pop (ref string[] array)
+        {
+            string arr = array[array.Length-1];
+            Array.Resize(ref array,array.Length-1);
+            return arr;
+        }
+        public static int Pop (ref int[] array)
+        {
+            int arr = array[array.Length-1];
+            Array.Resize(ref array,array.Length-1);
+            return arr;
+        }
+        public static double Pop (ref double[] array)
+        {
+            double arr = array[array.Length-1];
+            Array.Resize(ref array,array.Length-1);
+            return arr;
+        }
+        public static decimal Pop (ref decimal[] array)
+        {
+            decimal arr = array[array.Length-1];
+            Array.Resize(ref array,array.Length-1);
+            return arr;
+        }
+        public static float Pop (ref float[] array)
+        {
+            float arr = array[array.Length-1];
+            Array.Resize(ref array,array.Length-1);
+            return arr;
+        }
     }
 }
